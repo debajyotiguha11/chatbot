5bb640776c7134b69a784d63f6211972bc89172f
Debjyoti Guha <debajyotiguha11@gmail.com>
2018-03-03 19:56:52 +0530

    procfile update

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ def ask():
 	        # print bot_response
 	        return jsonify({'status':'OK','answer':bot_response})
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+#if __name__ == "__app__":
+app.run(host='0.0.0.0', debug=True)
